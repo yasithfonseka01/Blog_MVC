9822d4c39492b58dc3d8d375bd430d803cd99dec
Yasith Fonseka <168076239+yasithfonseka01@users.noreply.github.com>
2024-05-14 14:18:32 +0530

    blog system 1st commit

---
+++ Blog_Site/Controllers/AdminTagsController.cs
@@ -0,0 +1,47 @@
+﻿using Blog_Site.Data;
+using Blog_Site.Models.Domain;
+using Blog_Site.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Site.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private DB_Context bB_Context;
+        public AdminTagsController(DB_Context  dB_Context)
+        {
+            this.bB_Context = dB_Context;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        //public IActionResult SubmitTag(AddTagRequest addTagRequest)
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //var name = Request.Form["name"];
+            //var displayName = Request.Form["displayName"];
+
+            //var name = addTagRequest.Name;
+            //var displayName = addTagRequest.DisplayName;
+
+            //Mapping addd tag request to tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            bB_Context.Tags.Add(tag);
+            bB_Context.SaveChanges();
+
+            return View("Add");
+
+        }
+    }
+}
---
+++ Blog_Site/Data/DB_Context.cs
@@ -0,0 +1,17 @@
+﻿using Blog_Site.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_Site.Data
+{
+    public class DB_Context : DbContext
+    {
+        public DB_Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+ 
+    }
+}
